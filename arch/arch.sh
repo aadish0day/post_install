d1d1b1fc6fa09437b7471a2e673da916266ef038
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-03-05 17:28:59 +0530

    Update arch.sh

--- arch/arch.sh
+++ arch/arch.sh
@@ -5,7 +5,7 @@
 sudo pacman -Syu --noconfirm
 
 # Install packages
-sudo pacman -S --noconfirm neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf doxygen flameshot npm
+sudo pacman -S --noconfirm neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf htop doxygen flameshot npm
 
 # For AUR helper installation (yay or paru), uncomment and adjust the following:
 # git clone https://aur.archlinux.org/yay.git
