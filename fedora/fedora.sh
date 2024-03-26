cd87a0c8ca3e37829c5362574261e6cec3d22877
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-03-26 20:05:36 +0530

    add

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -34,7 +34,7 @@ dnf groupupdate -y sound-and-video
 
 # Install packages
 echo "Installing packages..."
-dnf install -y ranger ncdu mpv neovim maven yt-dlp fzf git unzip nodejs flameshot htop npm xclip ueberzug highlight atool mediainfo 
+dnf install -y ranger ncdu mpv neovim maven yt-dlp fzf git unzip nodejs flameshot htop npm xclip ueberzug highlight atool mediainfo neofetch android-tools
 
 echo "Installation and setup complete on Fedora Linux."
 
