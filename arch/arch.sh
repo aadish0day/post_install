5b99cd5d0a430ecf894ed721f027d2b474f6a9ad
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-03-30 23:28:58 +0530

    add
    to make pacman faster

--- arch/arch.sh
+++ arch/arch.sh
@@ -4,6 +4,10 @@
 # Update system and packages
 sudo pacman -Syu --noconfirm
 
+sudo pacman -S reflector
+
+sudo reflector --latest 5 --country India --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
 # Install packages
 sudo pacman -S --noconfirm neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf htop doxygen flameshot npm xclip ueberzug highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb zathura-epub obs-studio
 
