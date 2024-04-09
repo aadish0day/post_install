80c0f3afedc1144c07541cdfb8f0b4e7b359b418
Aadishx07 <aadishdas7@gmail.com>
2024-04-09 20:44:41 +0530

    add

--- arch/arch.sh
+++ arch/arch.sh
@@ -11,12 +11,5 @@ sudo reflector --latest 5 --country India --protocol http --protocol https --sor
 # Install packages
 sudo pacman -S --noconfirm neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf htop doxygen flameshot npm xclip ueberzug highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb obs-studio picom nitrogen starship xss-lock 
 
-# For AUR helper installation (yay or paru), uncomment and adjust the following:
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -si
-# cd ..
-# rm -rf yay
-
 
 echo "Installation and setup complete on Arch Linux."
--- arch/yay-install.sh
+++ arch/yay-install.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
+# Function to install yay
+install_yay() {
+    echo "yay could not be found, installing..."
+    
+    # Install necessary base-devel package group if not already installed
+    if ! pacman -Qq base-devel &> /dev/null; then
+        echo "Installing the base-devel package group..."
+        sudo pacman -S --needed base-devel
+    fi
+
+    # Clone yay repository
+    git clone https://aur.archlinux.org/yay.git
+    if [ $? -ne 0 ]; then
+        echo "Failed to clone yay repository."
+        exit 1
+    fi
+
+    # Change directory to yay
+    cd yay || exit
+
+    # Build and install yay
+    makepkg -si
+
+    # Clean up
+    cd ..
+    rm -rf yay
+
+    echo "yay has been successfully installed."
+}
+
+# Function to check and install a package if it's not installed
+install_package_if_missing() {
+    local package_name=$1
+    # Checking if the package is already installed
+    if ! yay -Qi "$package_name" &> /dev/null && ! pacman -Qi "$package_name" &> /dev/null; then
+        echo "Installing $package_name..."
+        yay -S --noconfirm "$package_name"
+    else
+        echo "$package_name is already installed."
+    fi
+}
+
+# Check if yay is installed, install it if not
+if ! command -v yay &> /dev/null; then
+    install_yay
+else
+    echo "yay is already installed."
+fi
+
 # Update system and databases
+echo "Updating system and databases..."
 yay -Syu --noconfirm
 
-# Install packages
-yay -S --noconfirm i3lock-color
-yay -S --noconfirm thorium-browser-bin
-yay -S --noconfirm vscodium-bin
-yay -S --noconfirm github-desktop-bin
-yay -S --noconfirm mocp
-yay -S --noconfirm ani-cli
-yay -S --noconfirm hakuneko-desktop
+# List of packages to install
+packages=(
+    i3lock-color
+    thorium-browser-bin
+    vscodium-bin
+    github-desktop-bin
+    moc
+    ani-cli
+    hakuneko-desktop
+)
 
-echo "Installation of packages completed!"
+# Loop through the list and install if missing
+for package in "${packages[@]}"; do
+    install_package_if_missing "$package"
+done
+
+echo "Installation process completed!"
 
