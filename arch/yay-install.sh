f6feef6212a73eba690f52a5a719c33be87b557c
Aadishx07 <aadishdas7@gmail.com>
2024-04-09 20:19:05 +0530

    add

--- arch/yay-install.sh
+++ arch/yay-install.sh
@@ -1,9 +1,16 @@
-yay -S i3lock-color
-yay -S thorium-browser-bin
-yay -S vscodium-bin
-yay -S github-desktop-bin
-yay -S mocp
-yay -S ani-cli
-yay -S hakuneko-desktop
+#!/bin/bash
 
+# Update system and databases
+yay -Syu --noconfirm
+
+# Install packages
+yay -S --noconfirm i3lock-color
+yay -S --noconfirm thorium-browser-bin
+yay -S --noconfirm vscodium-bin
+yay -S --noconfirm github-desktop-bin
+yay -S --noconfirm mocp
+yay -S --noconfirm ani-cli
+yay -S --noconfirm hakuneko-desktop
+
+echo "Installation of packages completed!"
 
