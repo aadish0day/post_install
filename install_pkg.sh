e9ba2b41bd3bbe19c81bd8d1d22b496d26bf3a60
Aadishx07 <aadishdas7@gmail.com>
2024-04-09 12:47:01 +0530

    add

--- install_pkg.sh
+++ install_pkg.sh
@@ -6,15 +6,33 @@ echo "2) Arch Linux"
 echo "3) Fedora"
 read -p "Distribution (1/2/3): " DISTRO_CHOICE
 
+# Define a function to install Nerd Fonts
+install_nerd_fonts() {
+  echo "Installing Nerd Fonts..."
+  ./nerd_font.sh
+}
+
+# Define a function to clone Neovim configuration
+clone_neovim_config() {
+  echo "Cloning Neovim configuration..."
+  git clone https://github.com/Aadishx07/neovim_config.git "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+}
+
+# Clone Neovim configuration before proceeding with the installation
+clone_neovim_config
+
 case $DISTRO_CHOICE in
   1)
-    cd debian && ./debian.sh
+    cd debian && ./debian.sh && cd ..
+    install_nerd_fonts
     ;;
   2)
-    cd arch && ./arch.sh
+    cd arch && ./arch.sh && cd ..
+    install_nerd_fonts
     ;;
   3)
-    cd fedora && ./fedora.sh
+    cd fedora && ./fedora.sh && cd ..
+    install_nerd_fonts
     ;;
   *)
     echo "Invalid selection. Exiting."
