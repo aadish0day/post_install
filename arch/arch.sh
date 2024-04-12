96fe4c7a9eecc5d3d6f6fbf2eb553adb24b8adfd
Aadishx07 <aadishdas7@gmail.com>
2024-04-12 13:11:25 +0530

    add
    Add the feature that if the app is already installed, it doesn't install again

--- arch/arch.sh
+++ arch/arch.sh
@@ -1,15 +1,33 @@
-
 #!/bin/bash
 
 # Update system and packages
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S reflector
+# Install reflector for managing mirror list
+sudo pacman -S --needed reflector
 
+# Configure mirrors for India
 sudo reflector --latest 5 --country India --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-# Install packages
-sudo pacman -S --noconfirm neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf htop doxygen flameshot npm xclip ueberzug highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb obs-studio picom nitrogen starship xss-lock qalculate-qt
+# Function to check and install packages if they are not already installed
+install_if_needed() {
+    for pkg in "$@"; do
+        if ! pacman -Qi $pkg &> /dev/null; then
+            echo "Installing $pkg..."
+            sudo pacman -S --noconfirm $pkg
+        else
+            echo "$pkg is already installed. Skipping..."
+        fi
+    done
+}
 
+# List of packages to install
+packages=(
+    neovim ranger ncdu mpv maven yt-dlp fzf git unzip nodejs ninja gettext libtool autoconf automake cmake gcc pkgconf htop doxygen flameshot npm xclip ueberzug highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb obs-studio picom nitrogen starship xss-lock qalculate-qt
+)
+
+# Install packages
+install_if_needed "${packages[@]}"
 
 echo "Installation and setup complete on Arch Linux."
+
