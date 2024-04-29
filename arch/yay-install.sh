706d761d37a2ab2e520f7cb24e38ccc7bd0ef456
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-04-29 21:25:30 +0530

    add

--- arch/yay-install.sh
+++ arch/yay-install.sh
@@ -3,11 +3,11 @@
 # Function to install yay
 install_yay() {
     echo "yay could not be found, installing..."
-    
+
     # Install necessary base-devel package group if not already installed
     if ! pacman -Qq base-devel &> /dev/null; then
         echo "Installing the base-devel package group..."
-        sudo pacman -S --needed base-devel
+        sudo pacman -S --needed base-devel --noconfirm
     fi
 
     # Clone yay repository
@@ -20,8 +20,8 @@ install_yay() {
     # Change directory to yay
     cd yay || exit
 
-    # Build and install yay
-    makepkg -si
+    # Build and install yay without confirmation
+    makepkg -si --noconfirm
 
     # Clean up
     cd ..
@@ -33,6 +33,8 @@ install_yay() {
 # Check if yay is installed
 if ! command -v yay &>/dev/null; then
     install_yay
+else
+    echo "yay is already installed."
 fi
 
 # Install packages
