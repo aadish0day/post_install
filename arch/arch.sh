848541fb32c2745e7ebe24355561730371242673
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-05-19 01:36:57 +0530

    add

--- arch/arch.sh
+++ arch/arch.sh
@@ -47,7 +47,7 @@ install_if_needed() {
 }
 
 # List of packages to install, removing any duplicates
-packages=(neovim ranger ncdu mpv maven yt-dlp fzf git nodejs gcc make ripgrep fd unzip htop gettext libtool doxygen flameshot npm xclip highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb obs-studio picom nitrogen starship xss-lock qalculate-qt libreoffice-still brightnessctl qbittorrent bluez bluez-utils blueman bat alacritty)
+packages=(neovim ranger ncdu mpv maven yt-dlp fzf git nodejs gcc make ripgrep fd unzip htop gettext libtool doxygen flameshot npm xclip highlight atool mediainfo neofetch android-tools img2pdf zathura zathura-pdf-poppler zathura-ps zathura-djvu zathura-cb obs-studio picom nitrogen starship xss-lock qalculate-qt libreoffice-still brightnessctl qbittorrent bluez bluez-utils blueman bat alacritty zsh)
 
 # Install packages
 install_if_needed "${packages[@]}"
@@ -55,6 +55,9 @@ install_if_needed "${packages[@]}"
 # Enable Bluetooth and display message
 sudo systemctl enable --now bluetooth.service
 echo "Bluetooth service has been enabled."
+
+echo "Change it to zsh"
+chsh $USER
 
 echo "Installation and setup complete on Arch Linux."
 
