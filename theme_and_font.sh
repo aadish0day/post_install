f0537c333642702a9426f3a6b001f846388eebbf
Aadishx07 <aadishdas7@gmail.com>
2024-08-26 23:11:06 +0530

    Adding some improvement

--- theme_and_font.sh
+++ theme_and_font.sh
@@ -1,77 +1,77 @@
-#!/bin/bash
-
-# Define the URL and destination directory for the GTK theme
-GTK_URL="https://github.com/dracula/gtk/archive/master.zip"
-GTK_DEST="/usr/share/themes"
-
-# Function to download the GTK theme
-download_gtk_theme() {
-	echo "Downloading Dracula GTK theme to $GTK_DEST..."
-	if sudo curl -L "$GTK_URL" -o "$GTK_DEST/theme.zip"; then
-		echo "Download successful."
-	else
-		echo "Failed to download the theme."
-		exit 1
-	fi
-}
-
-# Function to extract the GTK theme and clean up
-extract_gtk_theme() {
-	echo "Extracting files..."
-	if sudo unzip -q "$GTK_DEST/theme.zip" -d "$GTK_DEST"; then
-		echo "Files extracted."
-		sudo mv "$GTK_DEST/gtk-master" "$GTK_DEST/dracula-theme" && echo "Theme moved to final destination."
-		sudo rm "$GTK_DEST/theme.zip"
-		echo "Cleanup completed."
-	else
-		echo "Failed to extract files."
-		exit 1
-	fi
-}
-
-# Main execution flow for GTK theme
-download_gtk_theme
-extract_gtk_theme
-
-# Additional check for leftover directory
-if [ -d "$GTK_DEST/gtk-master" ]; then
-	echo "Removing leftover directory..."
-	sudo rmdir --ignore-fail-on-non-empty "$GTK_DEST/gtk-master"
-fi
-
-echo "GTK theme installation completed successfully."
-
-# Define the repository URL and the directory name for the icon theme
-ICON_REPO_URL="https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git"
-ICON_DIR_NAME="papirus-icon-theme"
-
-# Function to clone the icon theme repository
-clone_icon_repo() {
-	echo "Cloning Papirus icon theme from $ICON_REPO_URL..."
-	if git clone "$ICON_REPO_URL"; then
-		echo "Repository cloned successfully."
-	else
-		echo "Failed to clone the repository."
-		exit 1
-	fi
-}
-
-# Function to install the icon theme
-install_icon_theme() {
-	if [ -d "$ICON_DIR_NAME" ]; then
-		echo "Installing Papirus icon theme..."
-		cd "$ICON_DIR_NAME" && sudo ./install.sh
-		echo "Installation complete!"
-	else
-		echo "Directory $ICON_DIR_NAME does not exist."
-		exit 1
-	fi
-}
-
-# Main execution flow for icon theme
-clone_icon_repo
-install_icon_theme
-
+# #!/bin/bash
+#
+# # Define the URL and destination directory for the GTK theme
+# GTK_URL="https://github.com/dracula/gtk/archive/master.zip"
+# GTK_DEST="/usr/share/themes"
+#
+# # Function to download the GTK theme
+# download_gtk_theme() {
+# 	echo "Downloading Dracula GTK theme to $GTK_DEST..."
+# 	if sudo curl -L "$GTK_URL" -o "$GTK_DEST/theme.zip"; then
+# 		echo "Download successful."
+# 	else
+# 		echo "Failed to download the theme."
+# 		exit 1
+# 	fi
+# }
+#
+# # Function to extract the GTK theme and clean up
+# extract_gtk_theme() {
+# 	echo "Extracting files..."
+# 	if sudo unzip -q "$GTK_DEST/theme.zip" -d "$GTK_DEST"; then
+# 		echo "Files extracted."
+# 		sudo mv "$GTK_DEST/gtk-master" "$GTK_DEST/dracula-theme" && echo "Theme moved to final destination."
+# 		sudo rm "$GTK_DEST/theme.zip"
+# 		echo "Cleanup completed."
+# 	else
+# 		echo "Failed to extract files."
+# 		exit 1
+# 	fi
+# }
+#
+# # Main execution flow for GTK theme
+# download_gtk_theme
+# extract_gtk_theme
+#
+# # Additional check for leftover directory
+# if [ -d "$GTK_DEST/gtk-master" ]; then
+# 	echo "Removing leftover directory..."
+# 	sudo rmdir --ignore-fail-on-non-empty "$GTK_DEST/gtk-master"
+# fi
+#
+# echo "GTK theme installation completed successfully."
+#
+# # Define the repository URL and the directory name for the icon theme
+# ICON_REPO_URL="https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git"
+# ICON_DIR_NAME="papirus-icon-theme"
+#
+# # Function to clone the icon theme repository
+# clone_icon_repo() {
+# 	echo "Cloning Papirus icon theme from $ICON_REPO_URL..."
+# 	if git clone "$ICON_REPO_URL"; then
+# 		echo "Repository cloned successfully."
+# 	else
+# 		echo "Failed to clone the repository."
+# 		exit 1
+# 	fi
+# }
+#
+# # Function to install the icon theme
+# install_icon_theme() {
+# 	if [ -d "$ICON_DIR_NAME" ]; then
+# 		echo "Installing Papirus icon theme..."
+# 		cd "$ICON_DIR_NAME" && sudo ./install.sh
+# 		echo "Installation complete!"
+# 	else
+# 		echo "Directory $ICON_DIR_NAME does not exist."
+# 		exit 1
+# 	fi
+# }
+#
+# # Main execution flow for icon theme
+# clone_icon_repo
+# install_icon_theme
+#
 # # Define the directory to store fonts
 # FONTS_DIR="${HOME}/.local/share/fonts"
 #
