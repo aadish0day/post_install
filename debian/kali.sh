841a81bd33c18febd66fcc73c908368a7f1086cb
Aadishx07 <aadishdas7@gmail.com>
2025-07-27 00:48:12 +0530

    fix(kali):make the Script better

--- debian/kali.sh
+++ debian/kali.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-sudo apt install nala -y 
+# Install nala and update/upgrade system
+sudo apt install nala -y
 sudo nala update && sudo nala upgrade -y
+
+# Install necessary packages
 sudo nala install git stow zsh tmux curl wget vim neovim fzf starship zoxide -y
+
+# Remove existing .zshrc if it exists
 if [ -f ~/.zshrc ]; then
-    echo "Removing existing .zshrc file..."
-    rm ~/.zshrc
+	echo "Removing existing .zshrc file..."
+	rm ~/.zshrc
 else
-    echo ".zshrc file does not exist, nothing to remove."
+	echo ".zshrc file does not exist, nothing to remove."
 fi
 
-git clone https://github.com/aadish0day/dotfile.git ~/dotfile
+# Clone dotfiles only if it doesn't already exist
+if [ -d ~/dotfile ]; then
+	echo "Directory ~/dotfile already exists. Skipping clone."
+else
+	echo "Cloning dotfiles..."
+	git clone https://github.com/aadish0day/dotfile.git ~/dotfile
+fi
 
-cd ~/dotfile 
-
+# Navigate and link
+cd ~/dotfile
 ./link.sh
