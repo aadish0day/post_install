cbec317668c9f2a54aee760085fa7f3918169027
Aadishx07 <aadishdas7@gmail.com>
2025-11-08 21:50:08 +0530

    feat(arch): add kdeconnect

--- arch/arch.sh
+++ arch/arch.sh
@@ -269,7 +269,7 @@ x11_tilling_depen=(
 # List of KDE Plasma desktop environment packages
 kde_plasma_packages=(
     rsync obsidian elisa gwenview kamoso okular libreoffice-fresh wl-clipboard qt6-tools
-    mesa libva-mesa-driver libva-utils vulkan-radeon vulkan-tools dosfstools sshfs
+    mesa libva-mesa-driver libva-utils vulkan-radeon vulkan-tools dosfstools sshfs kdeconnect
 )
 
 # List of AUR packages
