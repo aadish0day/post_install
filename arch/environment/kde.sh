da08a318d631595c4329e076ca0c86e3c2382628
Aadishx07 <aadishdas7@gmail.com>
2026-04-21 20:26:53 +0530

    refactor(arch): update kde package list in kde.sh

--- arch/environment/kde.sh
+++ arch/environment/kde.sh
@@ -5,5 +5,5 @@ kde_plasma_packages=(
     plasma-desktop plasma-meta
     rsync obsidian elisa gwenview kamoso okular libreoffice-fresh wl-clipboard qt6-tools
     mesa libva-mesa-driver libva-utils vulkan-radeon vulkan-tools dosfstools sshfs kdeconnect
-    kclock
+    kclock plasma-meta 
 )
